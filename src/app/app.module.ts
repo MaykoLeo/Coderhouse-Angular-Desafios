8820e7b15eb31c8514ce31c505f70dba708f6d4d
Mayko Matus <maykoleo90@gmail.com>
2023-01-19 23:28:12 -0300

    imagenes agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentAreaComponent } from './components/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
