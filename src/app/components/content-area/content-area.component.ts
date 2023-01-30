4b01a218ab4b432564c170548169240148d838fb
Mayko Matus <maykoleo90@gmail.com>
2023-01-30 15:49:54 -0300

    branch segundo desafio resubido

--- src/app/components/content-area/content-area.component.ts
+++ src/app/components/content-area/content-area.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Estudiante } from 'src/app/models/estudiante';
 
 @Component({
   selector: 'app-content-area',
   templateUrl: './content-area.component.html',
   styleUrls: ['./content-area.component.css']
 })
-export class ContentAreaComponent {
 
+export class ContentAreaComponent {
+  estudianteActual: Estudiante = {
+    nombre: 'Julian',
+    edad: 28,
+    estado: true,
+  };
+
+  listaEstudiante: Array<Estudiante> = [
+    {
+      nombre: 'Luis',
+      edad: 26,
+      estado: true,
+    },
+    {
+      nombre: 'Ana',
+      edad: 21,
+      estado: false,
+    },
+    {
+      nombre: 'Jorge',
+      edad: 29,
+      estado: false,
+    },
+    {
+      nombre: 'Rocio',
+      edad: 22,
+      estado: true,
+    }
+  ]
 }
